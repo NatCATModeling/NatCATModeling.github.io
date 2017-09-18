28e395245db91136386bfceda59795d09a9f932e
NatCATModeling <NatCATModeling@gmail.com>
2017-09-18 14:01:10 -0700

    Diatance measure added
    *. Added distance measure tool
    **. Fixed the fault URL link in USGS

--- js/index.js
+++ js/index.js
@@ -21,6 +21,24 @@ mapboxgl.accessToken = "pk.eyJ1IjoibmF0Y2F0bW9kZWxpbmciLCJhIjoiY2lmcHBibmI5NmQ2d
             zoom: 6, // starting zoom
             maxBounds: bounds // Sets bounds as max
         });
+
+        //measure distance
+        var distanceContainer = document.getElementById('distance');
+
+        // GeoJSON object to hold our measurement features
+        var geojson = {
+            "type": "FeatureCollection",
+            "features": []
+        };
+
+        // Used to draw a line between points
+        var linestring = {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": []
+            }
+        };
     }
 
   // Append data attribution
@@ -511,9 +529,8 @@ map.on("mousemove", function(e) {
         // Populate the popup and set its coordinates based on the feature found.
         popup.setLngLat(e.lngLat)
             //.setHTML("<strong>Highlighted Fault Line(s):</br><hr>" + features[0].properties.name, null, 2 + "</strong>")
-            .setHTML("<span style='color:cyan;font-weight:bold;font-size: 12pt' >" + "<a href ='" + features[0].properties.CFM_URL + "' target='_blank'>" + features[0].properties.name + " </a>" + "</span>")
+            .setHTML("<span style='color:cyan;font-weight:bold;font-size: 12pt' >" + "<a href ='" + features[0].properties.CFM_URL.replace("geohazards.","earthquake.") + "' target='_blank'>" + features[0].properties.name + " </a>" + "</span>")
             .addTo(map);
-
 
         // hightlighted fault segment
         map.setFilter("USGSfaults-hover", ["==", "name", features[0].properties.name]);
@@ -521,8 +538,94 @@ map.on("mousemove", function(e) {
         map.setFilter("USGSfaults-hover", ["==", "name", ""]);
     }
 
+    // UI indicator for clicking/hovering a point on the map to measure distance
+    var features = map.queryRenderedFeatures(e.point, { layers: ['measure-points'] });
+    //map.getCanvas().style.cursor = (features.length) ? 'pointer' : 'crosshair';
 });
 
+//measure distance
+map.on('load', function() {
+    map.addSource('geojson', {
+        "type": "geojson",
+        "data": geojson
+    });
+
+    // Add styles to the map
+    map.addLayer({
+        id: 'measure-points',
+        type: 'circle',
+        source: 'geojson',
+        paint: {
+            'circle-radius': 5,
+            'circle-color': '#000'
+        },
+        filter: ['in', '$type', 'Point']
+    });
+    map.addLayer({
+        id: 'measure-lines',
+        type: 'line',
+        source: 'geojson',
+        layout: {
+            'line-cap': 'round',
+            'line-join': 'round'
+        },
+        paint: {
+            'line-color': '#000',
+            'line-width': 2.5
+        },
+        filter: ['in', '$type', 'LineString']
+    });
+
+    map.on('click', function(e) {
+        var features = map.queryRenderedFeatures(e.point, { layers: ['measure-points'] });
+
+        // Remove the linestring from the group
+        // So we can redraw it based on the points collection
+        if (geojson.features.length > 1) geojson.features.pop();
+
+        // Clear the Distance container to populate it with a new value
+        distanceContainer.innerHTML = '';
+
+        // If a feature was clicked, remove it from the map
+        if (features.length) {
+            var id = features[0].properties.id;
+            geojson.features = geojson.features.filter(function(point) {
+                return point.properties.id !== id;
+            });
+        } else {
+            var point = {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        e.lngLat.lng,
+                        e.lngLat.lat
+                    ]
+                },
+                "properties": {
+                    "id": String(new Date().getTime())
+                }
+            };
+
+            geojson.features.push(point);
+        }
+
+        if (geojson.features.length > 1) {
+            linestring.geometry.coordinates = geojson.features.map(function(point) {
+                return point.geometry.coordinates;
+            });
+
+            geojson.features.push(linestring);
+
+            // Populate the distanceContainer with total distance
+            var value = document.createElement('pre');
+            value.textContent = 'Distance: ' + turf.lineDistance(linestring, 'miles').toLocaleString() + 'mi';
+            distanceContainer.appendChild(value);
+        }
+
+        map.getSource('geojson').setData(geojson);
+    });
+});
 
 // Base Map Selector
 form.styleSelect.addEventListener('change', function() {
